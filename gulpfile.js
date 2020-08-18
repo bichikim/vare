24024b80032729f4ccf7602d07850c19c68a00d8
Bichi Kim <bichi@live.co.kr>
2020-08-19 03:22:11 +0900

    remove useless files

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const {parallel} = require('gulp')
-const execa = require('gulp-execa')
-
-exports.devTsc = execa.task('ttsc --outDir ./dist -p tsconfig.bundle.json')
-
-exports.testJs = execa.task('')
