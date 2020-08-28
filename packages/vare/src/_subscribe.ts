43664a1f7f71c0d85fc8c6ff2097ab232a9072ce
Bichi Kim <bichi@live.co.kr>
2020-08-29 03:11:38 +0900

    update

--- packages/vare/src/_subscribe.ts
+++ packages/vare/src/_subscribe.ts
@@ -1,9 +1,65 @@
 import {AnyFunc, AnyObject} from './types'
 
+const withNest = <Pr extends AnyObject, F extends AnyFunc, P>(originalFunc: (props: Pr & NestProps<F, P>) => any, nest: Map<P, Map<F, true>>) => {
+  return (props: Pr & NestProps<F, P>) => {
+    originalFunc({...props, nest} as Pr & NestProps<F, P>)
+  }
+}
+
+const withLink = <Pr extends AnyObject> (originalFunc: (props: Pr & LinkProps) => any, link) => {
+  return (props: Pr) => {
+    originalFunc({...props, link} as Pr & LinkProps)
+  }
+}
+
+interface LinkProps {
+  link?: Map<Subscribe<any, any>, true>
+}
+
+interface NestProps<F, P> {
+  nest?: Map<P, Map<F, true>>
+}
+
 interface SubscribeProps<F, P> {
-  nest: Map<P, Map<F, true>>
-  link: Map<SubscribeProps<any, any>, true>
-  defaultType: P
+  nest?: Map<P, Map<F, true>>
+  type?: P
+  func?: F
+}
+
+const typedSubscribe = <F extends AnyFunc, P>(props: SubscribeProps<F, P>) => {
+  const {nest, type, func} = props
+  if (!nest || !type || !func) {
+    return
+  }
+  const subscribes = nest.get(type)
+  if (subscribes) {
+    subscribes.set(func, true)
+  }
+}
+
+const typedUnsubscribe = <F extends AnyFunc, P>(props: SubscribeProps<F, P>) => {
+  const {nest, type, func} = props
+
+  if (!nest || !type || !func) {
+    return
+  }
+
+  const subscribes = nest.get(type)
+  if (subscribes) {
+    subscribes.delete(func)
+  }
+}
+
+interface WithSubscribes<F extends AnyFunc | AnyObject> {
+  subscribes?: Map<F, true>
+  execute?: (target: F, ...args: any[]) => any
+  args?: any[]
+}
+
+export const withSubscribes = <F extends AnyFunc | AnyObject>(originalFunc: (props: WithSubscribes<F>) => any, subscribes: Map<F, true>) => {
+  return (props: WithSubscribes<F>): any => {
+    return originalFunc({...props, subscribes})
+  }
 }
 
 const defaultExecute = <F extends AnyFunc | AnyObject>(target: F, ...args: any[]) => {
@@ -12,48 +68,38 @@ const defaultExecute = <F extends AnyFunc | AnyObject>(target: F, ...args: any[]
   }
 }
 
-export const callAllSubscribes = <F extends AnyFunc | AnyObject>(subscribes: Map<F, true>) =>
-  (execute: (target: F, ...args: any[]) => any = defaultExecute) =>
-    (
-      ...args: any[]
-    ): void => {
-      subscribes.forEach((_, subscribe) => {
-        execute(subscribe, ...args)
-      })
-    }
+export const callAllSubscribes = <F extends AnyFunc | AnyObject>(props: WithSubscribes<F>) => {
+  const {subscribes = [], execute = defaultExecute, args = []} = props
+  subscribes.forEach((_, subscribe) => {
+    execute(subscribe, ...args)
+  })
+}
 
-export const typedSubscribe = <F extends AnyFunc, P>(nest: Map<P, Map<F, true>>, defaultType: P) =>
-  (func: F, type: P = defaultType): void => {
-    const subscribes = nest.get(type)
-    if (subscribes) {
-      subscribes.set(func, true)
-    }
+interface TypedTriggerProps<F extends AnyFunc, P> extends SubscribeProps<F, P>, Omit<WithSubscribes<F>, 'subscribes'>, LinkProps {
+  args?: any[]
+}
+
+export const typedTrigger = <F extends AnyFunc, P>(props: TypedTriggerProps<F, P>): void => {
+  const {args = [], nest, type, execute, func, link} = props
+
+  if (!nest || !type || !func) {
+    return
   }
 
-export const typedUnsubscribe = <F, P>(nest: Map<P, Map<F, true>>, defaultType: P) =>
-  (func: F, type: P = defaultType): void => {
-    const subscribes = nest.get(type)
-    if (subscribes) {
-      subscribes.delete(func)
-    }
+  const subscribes = nest.get(type)
+
+  if (!subscribes) {
+    return
   }
 
-export const typedTrigger = <F extends AnyFunc, P>(nest: Map<P, Map<F, true>>) =>
-  (callback: (type: P, ...args: any[]) => any) =>
-    (
-      type: P,
-      ...args: any[]
-    ): void => {
-      const subscribes = nest.get(type)
+  callAllSubscribes({subscribes, execute, args})
 
-      if (!subscribes) {
-        return
-      }
-
-      callAllSubscribes<F>(subscribes)()(...args)
-
-      callback(type, ...args)
-    }
+  if (link) {
+    link.forEach((_, subscribe) => {
+      subscribe.trigger(type, ...args)
+    })
+  }
+}
 
 export const typedClear = <F extends AnyFunc, P>(nest: Map<P, Map<F, true>>) => (type: P): void => {
   const subscribes = nest.get(type)
@@ -79,19 +125,15 @@ export const createSubscribe = <F extends AnyFunc, P>(types: P[], defaultType: P
     nest.set(type, new Map<F, true>())
   })
 
-  const linkExecute = (subscribe, type, ...args) => (subscribe.trigger(type, ...args))
+  const triggerFunc = withLink<TypedTriggerProps<F, P>>(withNest(typedTrigger, nest), link)
+  const subscribeFunc = withNest(typedSubscribe, nest)
+  const unsubscribeFunc = withNest(typedUnsubscribe, nest)
 
-  const callAllLinks = callAllSubscribes<Subscribe<any, any>>(link)(linkExecute)
+  const trigger = (type: P, ...args: any[]) => triggerFunc({type, args})
 
-  const afterTrigger = (type: P, ...args: any[]) => {
-    callAllLinks(type, ...args)
-  }
+  const subscribe = (func: F, type: P = defaultType) => subscribeFunc({func, type})
 
-  const trigger = typedTrigger<F, P>(nest)(afterTrigger)
-
-  const subscribe = typedSubscribe<F, P>(nest, defaultType)
-
-  const unsubscribe = typedUnsubscribe<F, P>(nest, defaultType)
+  const unsubscribe = (func: F, type: P = defaultType) => unsubscribeFunc({func, type})
 
   const clear = typedClear<F, P>(nest)
 
