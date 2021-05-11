6edcfe4ac8e3d0a9c19d045352eb9083df2d108c
bichikim <bichi@live.co.kr>
2021-05-11 16:52:38 +0900

    update tests & emotion

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,24 @@
 module.exports = {
+  presets: [
+    '@vue/babel-preset-app',
+    ['@babel/preset-typescript', {
+      allExtensions: true,
+      isTSX: true,
+    }],
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@': './src',
+      },
+      cwd: 'packagejson',
+      loglevel: 'info',
+    }],
+  ],
   env: {
     test: {
       plugins: [
-        [
-          'module-resolver',
-          {
-            alias: {
-              src: './src',
-              '@': './src',
-            },
-            cwd: 'packagejson',
-            loglevel: 'info',
-          },
-        ],
-        // ['@babel/plugin-transform-react-jsx'],
-      ],
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            // for tree shaking
-            targets: {
-              node: true,
-            },
-          },
-        ],
-        ['@babel/preset-typescript', {
-          allExtensions: true,
-          isTSX: true,
-        }],
+        'istanbul',
       ],
     },
   },
